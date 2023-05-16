54c0f5acc6608b60f7daa176e653db3b0cbfdc98
HighestDev <kenneth.danso@amalitech.org>
2023-05-16 09:38:23 +0000

    modifying pages for templates views

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from . import views
 
 app_name = 'accounts'
 
 urlpatterns = [
-
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('customer/',views.customer,name='customer'),
+    path('products/',views.products,name='product')
 ]
