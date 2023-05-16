9c50403b926a968ec3d3ac42d74609553501dfcd
HighestDev <kenneth.danso@amalitech.org>
2023-05-16 10:33:01 +0000

    order model created which relates to both products and customer

--- accounts/models.py
+++ accounts/models.py
@@ -17,12 +17,26 @@ class Customer(models.Model):
 
 
 class Product(models.Model):
+    class Category(models.TextChoices):
+        IN_DOOR='IN DOOR'
+        OUT_DOOR='OUT DOOR'
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='customer')
     name = models.CharField(max_length=30)
     price = models.FloatField()
-    category = models.CharField(max_length=5)
+    category = models.CharField(max_length=15, choices=Category.choices, default=Category.IN_DOOR)
     description = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
+
+
+class Order(models.Model):
+    class Status(models.TextChoices):
+        PENDING = 'OUT DOOR'
+        DELIVERED = 'DELIVERED'
+        OUT_OF_STOCK = 'OUT OF STOCK'
+
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='order_customer')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='order_product')
+    status = models.CharField(max_length=30, choices=Status.choices, default=Status.PENDING)
