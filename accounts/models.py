12cc92748dab17773bc179cc2a33fad8cb5bd502
HighestDev <kenneth.danso@amalitech.org>
2023-05-16 10:16:03 +0000

    customer model created and registered in admin.py file

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    name=models.CharField(max_length=30)
+    phone=models.CharField(max_length=13, null=True)
+    email=models.EmailField(null=True)
+    date_created=models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-date_created']
+        indexes=[models.Index(fields=['-date_created'])]
+
+    def __str__(self):
+        return self.name
