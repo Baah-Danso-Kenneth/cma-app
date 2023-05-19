aca51618f58b76c8ed150f9e5fcc80ed65bed8fd
HighestDev <kenneth.danso@amalitech.org>
2023-05-19 01:46:26 +0000

    email configuration added to the settings file

--- crm/settings.py
+++ crm/settings.py
@@ -125,3 +125,10 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+#Email Server Configuration
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_HOST_USER='dansobaahkenneth@gmail.com'
+EMAIL_HOST_PASSWORD='aceezpmbczisrwxp'
+EMAIL_PORT=587
+EMAIL_USE_TLS=True
