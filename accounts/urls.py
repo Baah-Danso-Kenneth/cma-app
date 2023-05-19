0e4eff9e5ea28a7c03000bbbf01f5a1ea301c615
HighestDev <kenneth.danso@amalitech.org>
2023-05-19 01:45:17 +0000

    using django inbuilt functionalities to handle password reset process

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_view
+
 
 app_name = 'accounts'
 
@@ -13,6 +15,12 @@ urlpatterns = [
     path('update-order/<str:pk>/',views.update_order_form, name='update_order'),
     path('delete-order/<str:pk>/',views.delete_order_form, name='delete_order'),
 
+    #password-reset,
+    path('reset_password/',auth_view.PasswordResetView.as_view(),name='reset_password'),
+    path('reset_password_sent/', auth_view.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uid64>/<token>/', auth_view.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
+    path('reset_password/',auth_view.PasswordResetConfirmView.as_view,name='password_reset_complete'),
+    
     #authentication
     path('register/users',views.registerUser, name='register'),
     path('login/users',views.loginUser, name='login'),
